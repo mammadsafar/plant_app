25bc963e5c3281c912910cead0c6fa6948488a70
Mohammad Safarzadeh <mohammadsafar2014@gmail.com>
2024-02-08 13:01:56 +0330

    add QR Code Scanner

---
+++ lib/screens/camera_page.dart
@@ -0,0 +1,94 @@
+import 'package:flutter/material.dart';
+
+import 'package:mobile_scanner/mobile_scanner.dart';
+import 'package:plant_app/const/constants.dart';
+import 'package:plant_app/screens/qr_overlay.dart';
+
+class CameraPage extends StatefulWidget {
+  const CameraPage({super.key});
+
+  @override
+  State<CameraPage> createState() => _CameraPageState();
+}
+
+class _CameraPageState extends State<CameraPage> {
+  MobileScannerController cameraController = MobileScannerController();
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Stack(
+        children: [
+          MobileScanner(
+            allowDuplicates: false,
+            onDetect: (barcode, args) {
+              if (barcode.rawValue == null) {
+                debugPrint('Failed to Scan Barcode');
+              } else {
+                final String code = barcode.rawValue!;
+                debugPrint('Scanned Barcode: $code');
+              }
+            },
+          ),
+
+          QRScannerOverlay(overlayColour: Colors.black.withOpacity(0.5)),
+          // AppBar
+          Positioned(
+            top: 50,
+            left: 20,
+            right: 20,
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                // x Button
+                GestureDetector(
+                  onTap: () {
+                    Navigator.pop(context);
+                  },
+                  child: Container(
+                    height: 40,
+                    width: 40,
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(50),
+                      color: Constants.primaryColor.withOpacity(0.2),
+                    ),
+                    child: const Icon(
+                      Icons.close,
+                      color: Colors.white,
+                    ),
+                  ),
+                ),
+                // Torch Button
+                Container(
+                  height: 40,
+                  width: 40,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(50),
+                    color: Constants.primaryColor.withOpacity(0.2),
+                  ),
+                  child: IconButton(
+                    icon: ValueListenableBuilder(
+                      valueListenable: cameraController.torchState,
+                      builder: (context, state, child) {
+                        switch (state) {
+                          case TorchState.off:
+                            return const Icon(Icons.flash_off,
+                                color: Colors.white);
+                          case TorchState.on:
+                            return const Icon(Icons.flash_on,
+                                color: Colors.yellow);
+                        }
+                      },
+                    ),
+                    onPressed: () {
+                      cameraController.toggleTorch();
+                    },
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
--- lib/screens/scan_page.dart
+++ lib/screens/scan_page.dart
@@ -1,4 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:page_transition/page_transition.dart';
+import 'package:plant_app/const/constants.dart';
+
+import 'package:mobile_scanner/mobile_scanner.dart';
+import 'package:plant_app/screens/camera_page.dart';
 
 class ScanPage extends StatefulWidget {
   const ScanPage({super.key});
@@ -8,10 +13,97 @@ class ScanPage extends StatefulWidget {
 }
 
 class _ScanPageState extends State<ScanPage> {
+  MobileScannerController cameraController = MobileScannerController();
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(child: Text('Scan Weed')),
+    Size size = MediaQuery.of(context).size;
+    return Scaffold(
+      body: Stack(
+        children: [
+          // AppBar
+          Positioned(
+            top: 50,
+            left: 20,
+            right: 20,
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                // x Button
+                GestureDetector(
+                  onTap: () {
+                    Navigator.pop(context);
+                  },
+                  child: Container(
+                    height: 40,
+                    width: 40,
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(50),
+                      color: Constants.primaryColor.withOpacity(0.3),
+                    ),
+                    child: Icon(
+                      Icons.close,
+                      color: Constants.primaryColor,
+                    ),
+                  ),
+                ),
+                // Like Button
+                Container(
+                  height: 40,
+                  width: 40,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(50),
+                    color: Constants.primaryColor.withOpacity(0.3),
+                  ),
+                  child: Icon(
+                    Icons.share,
+                    color: Constants.primaryColor,
+                  ),
+                ),
+              ],
+            ),
+          ),
+          Positioned(
+            top: 100,
+            left: 20,
+            right: 20,
+            child: SizedBox(
+              width: size.width * 0.8,
+              height: size.height * 0.8,
+              child: Center(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    GestureDetector(
+                      onTap: () {
+                        Navigator.push(
+                            context,
+                            PageTransition(
+                                child: const CameraPage(),
+                                type: PageTransitionType.fade));
+                      },
+                      child: Image.asset(
+                        'assets/images/code-scan.png',
+                        height: 100,
+                      ),
+                    ),
+                    const SizedBox(height: 20),
+                    Text(
+                      'برای اسکن گیاه کلیک کنید',
+                      style: TextStyle(
+                        color: Constants.primaryColor,
+                        fontFamily: 'Lalezar',
+                        fontSize: 18,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
